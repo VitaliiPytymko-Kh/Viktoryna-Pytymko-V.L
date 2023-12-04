b67f5211ca67b91c61c6cb505358adc6d743206c
VitaliiPytymko <vitaliipytymko@gmail.com>
2023-12-05 00:14:38 +0200

    змінено class Question

--- Viktoryna Pytymko V.L/Question.cs
+++ Viktoryna Pytymko V.L/Question.cs
@@ -8,11 +8,11 @@ namespace Viktoryna_Pytymko_V.L
 {
     public class Question
     {
-        public string Category { get; set; }
-        public string Text { get; set; }
-        public Dictionary<string, bool> Options { get; set; }
-        public Question(string category, string text, Dictionary<string, bool> options)
+        public string Category { get; }
+        public string Text { get; }
+        public Dictionary<string, bool> Options { get; }
 
+        private Question(string category, string text, Dictionary<string, bool> options)
         {
             Category = category;
             Text = text;
@@ -27,32 +27,21 @@ namespace Viktoryna_Pytymko_V.L
 
                 if (parts.Length < 3)
                 {
-                    Console.WriteLine($"Invalid format: {data}");
+                    Console.WriteLine($"Invalid format: {data}. Insufficient parts.");
                     return null;
                 }
 
                 string category = parts[0];
                 string text = parts[1];
 
-                Dictionary<string, bool> options = new Dictionary<string, bool>();
+                Dictionary<string, bool> options = ParseOptions(parts[2]);
 
-                string[] optionsPart = parts[2].Split(',');
-
-                for (int i = 0; i < optionsPart.Length; i += 2)
+                if (options == null)
                 {
-                    string optionText = optionsPart[i];
-                    string optionValue = optionsPart[i + 1];
-                    optionValue = optionValue.TrimEnd(';');
-                    if (bool.TryParse(optionValue, out bool result))
-                    {
-                        options.Add(optionText, result);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Error parsing option data: {data}");
-                        return null;
-                    }
+                    Console.WriteLine($"Error parsing options for data: {data}");
+                    return null;
                 }
+
                 return new Question(category, text, options);
             }
             catch (Exception ex)
@@ -62,12 +51,34 @@ namespace Viktoryna_Pytymko_V.L
             }
         }
 
+        private static Dictionary<string, bool> ParseOptions(string optionsPart)
+        {
+            Dictionary<string, bool> options = new Dictionary<string, bool>();
+            string[] optionParts = optionsPart.Split(',');
+
+            for (int i = 0; i < optionParts.Length; i += 2)
+            {
+                string optionText = optionParts[i];
+                string optionValue = optionParts[i + 1].TrimEnd(';');
+
+                if (bool.TryParse(optionValue, out bool result))
+                {
+                    options.Add(optionText, result);
+                }
+                else
+                {
+                    return null; // Indicate failure in parsing options
+                }
+            }
+
+            return options;
+        }
+
         public override string ToString()
         {
             string optionsString = string.Join(",", Options.Select(kv => $"{kv.Key},{kv.Value}"));
             return $"{Category};{Text};{optionsString}";
         }
-
     }
 }
 
